a449f046595d1350aaa2c72d81080457853932a4
Roger <rogercr2002@hotmail.com>
2024-07-13 22:39:05 -0600

    deploy redirect

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,20 @@ import path from 'path';
 export default defineConfig({
   root: path.resolve(__dirname, 'src'),
   build: {
-    outDir: '../dist',
+    outDir: path.resolve(__dirname, 'dist'),
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'src/index.html'),
+        mealPlanner: path.resolve(__dirname, 'src/meal-planner.html'),
+        recipeSearch: path.resolve(__dirname, 'src/recipe-search.html'),
+      },
+    },
   },
   server: {
     port: 5173,
     open: true,
     strictPort: true,
+    historyApiFallback: true,
   },
 });
